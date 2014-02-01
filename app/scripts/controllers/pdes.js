75310b1437ec773eb27545d8142b41a1a0afa59e
Harish Narayanan <hnarayanan@gmail.com>
2014-02-01 23:42:24 +0100

    Try to use api as the data source instead of mock json

--- app/scripts/controllers/pdes.js
+++ app/scripts/controllers/pdes.js
@@ -2,7 +2,7 @@
 
 angular.module('pdezengardenApp')
   .controller('PdesCtrl', function ($scope, $http) {
-      $http.get('data/pdes/pdes.json').success(function(data) {
+      $http.get('http://localhost:8000/pdes/').success(function(data) {
           $scope.pdes = data;
         });
     });
