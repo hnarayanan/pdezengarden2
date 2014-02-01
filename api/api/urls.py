1d64e0a46cbb4d3fac1e26e98434892fbf5becd8
Harish Narayanan <hnarayanan@gmail.com>
2014-02-01 23:26:21 +0100

    Clean ups to allow for thumbnail upload

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib import admin
 from django.conf.urls import patterns, url, include
 
 from rest_framework.routers import DefaultRouter
@@ -5,10 +7,21 @@ from rest_framework.routers import DefaultRouter
 from pdes.views import PDEViewSet
 
 
+admin.autodiscover()
+
 router = DefaultRouter()
 router.register(r'pdes', PDEViewSet)
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
+
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
--- api/pdes/models.py
+++ api/pdes/models.py
@@ -9,7 +9,7 @@ class PDE(TimeStampedModel):
     name = models.CharField(max_length=255)
     tagline = models.CharField(max_length=500)
     slug = models.SlugField(max_length=255)
-    thumbnail = models.ImageField(upload_to='/media')
+    thumbnail = models.ImageField(upload_to='thumbnails/')
 
     def __unicode__(self):
         return self.name
