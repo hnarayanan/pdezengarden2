a1e7ea74097f46fb1436869f786fff6c95b17e1d
Harish Narayanan <hnarayanan@gmail.com>
2014-02-03 21:51:31 +0100

    Include MathJax

--- app/scripts/controllers/pde.js
+++ app/scripts/controllers/pde.js
@@ -2,6 +2,6 @@
 
 angular.module('pdezengardenApp')
   .controller('PdeCtrl', function($scope, $routeParams, Pde) {
-//      MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
       $scope.pde = Pde.get({pdeId: $routeParams.pdeId});
    });
